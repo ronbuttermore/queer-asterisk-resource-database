a3d92656f849695d9b21ecb3a8b5504b963684e4
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-07-29 17:43:43 -0600

    createdsessionforlogin

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const sess = {
+  secret: uuidv4(), 
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+    table: ''
+  }),
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
