e43302546055e8b1c1dea7741b9f6207c9eacba3
Ron Buttermore <ron.buttermore@gmail.com>
2023-08-03 19:21:50 -0600

    fixes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -11,6 +11,39 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/signup', async (req, res) => {
+  const userData = await User.create({
+    username: req.body.username,
+    password: req.body.password
+});
+res.status(200).json(resourceData);
+})
 
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Username and password are required' });
+    }
+
+    const user = await User.findOne({ where: { username } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    res.status(200).json({ message: 'Login successful' });
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).json({ error: 'An unexpected error occurred', errorMessage: error.message });
+  }
+});
 
 module.exports = router;
